dbc931e6bcccb4309885d91b94810de46abdc705
XingyouLee <xingyoulee599@gmail.com>
2025-10-27 18:32:17 +0100

    add db

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,16 +1,25 @@
+from datetime import date
+from .models import Post
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
 def starting_page(request):
-    return render(request, "blog/index.html")
-
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    return render(request, "blog/index.html", {
+      "posts": latest_posts
+    })
 
 
 def posts(request):
-    pass
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html", {
+      "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    pass
+    identified_post = Post.objects.get(slug=slug)
+    return render(request, "blog/post-detail.html", {
+      "post": identified_post,
+      "tags": identified_post.tag.all()
+    })
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-StATICFILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
